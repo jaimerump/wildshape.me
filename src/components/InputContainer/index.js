9c0814bfff06f06ccd08714b6311b8749eadee92
Jaime Rump <jaime.lee.rump@gmail.com>
2026-01-08 13:53:14 -0500

    Clean slate to test out Claude's capabilities

--- src/components/InputContainer/index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-// import cssVars from '../../cssVariables';
-
-export const InputContainer = styled.div.attrs({ className: 'input__container' })`
-  margin: 1em;
-`;
-
-export default InputContainer;
